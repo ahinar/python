b03e6878981bb6214e8a266d9ef80450edf5dcfa
Ahinardev <ahinar@gmail.com>
2025-03-01 15:49:16 -0500

    función saludar

---
+++ Udemy/funciones.py
@@ -0,0 +1,10 @@
+#funcion para saludar
+print('*** Funcion para saludar ***')
+
+# definimos la funcion
+def saludar (nombre):
+	print(f'Hola {nombre}')
+	
+nombre = input('cual es tu nombre? ')
+
+saludar(nombre)
